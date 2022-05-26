748bc897d171abcb7562bdd1208bfb8bf0d52761
hnuruzada <90691865+hnuruzada@users.noreply.github.com>
2022-05-26 14:11:31 +0400

    Create

---
+++ Ap103PartialView/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Ap103PartialView.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Image { get; set; }
+    }
+}
